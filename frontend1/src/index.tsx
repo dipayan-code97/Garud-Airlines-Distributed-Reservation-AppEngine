dd436df40d64cdb7829750992b88738d745e8cc6
Dipayan Paul <56677022+dipayan-code97@users.noreply.github.com>
2024-01-11 03:16:08 +0530

    Add files via upload

---
+++ frontend1/src/index.tsx
@@ -0,0 +1,18 @@
+// @ts-ignore
+import React from 'react';
+// @ts-ignore
+import ReactDOM from 'react-dom/client';
+import './styles/Index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
